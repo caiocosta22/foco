391a46d12f796d3b03ca72e83e7eb3fd2e27330d
Caio Costa <caiocosta22@gmail.com>
2024-12-12 20:43:28 -0300

    ajustes

--- src/components/ScrollEffect/ScrollEffect.vue
+++ src/components/ScrollEffect/ScrollEffect.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 const contents = ref([
   {
     img: "/images/home/logopic.png",
+    imgLogo: true,
     titulo: "Estude com FOCO",
     descricao: "Comece rapidamente criando uma conta e carregando o edital do concurso. Nossa plataforma vai guiar seus estudos, ajustando cada passo para otimizar seu aprendizado.",
     botaodesc: "Conheça agora"
@@ -12,7 +13,10 @@ const contents = ref([
     img: "/images/home/comunidade.png",
     titulo: "Comunidade FOCO",
     descricao: "Entre para a nossa comunidade online e conecte-se com outros estudantes que, assim como você, estão focados em conquistar a aprovação em concursos públicos. ",
-    botaodesc: "Entre aqui"
+    botaodesc: "Entre aqui",
+    backgroundAmarelo: true,
+    tituloRoxo: true,
+    botaoRoxo: true
   },
   {
     img: "/images/home/newsletter.jpg",
@@ -42,20 +46,29 @@ const contents = ref([
   <template
   v-for="content in contents"
   :key="content">
-    <div class="box">
-      <img :src="content.img" alt="">
+    <div class="box" :class="{bgamarelo: content.backgroundAmarelo}">
+      <img :src="content.img" :alt="content.titulo"
+      :class="{imgLogo: content.imgLogo}">
       <div class="conteudo">
-        <h3 class="text-lg text-bold">
+        <h3 class="text-lg text-bold texto-amarelo"
+        :class="{tituloroxo: content.tituloRoxo}">
           {{ content.titulo }}
         </h3>
-        <p class="text-md">
-          {{ content.descricao }}
+        <p class="text-md" :class="{tituloroxo: content.tituloRoxo}"
+        v-html="content.descricao">
         </p>
-        <a href="" class="botao-amarelo">
-          <span>
-            {{ content.botaodesc }}
-          </span>
-        </a>
+        <div class="flex">
+          <template v-if="content.optionmenu">
+            <template>
+            </template>
+          </template>
+          <a href="" class="botao-amarelo"
+          :class="{botaoroxo: content.botaoRoxo}">
+            <span class="text-sm">
+              {{ content.botaodesc }}
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   </template>
@@ -66,29 +79,57 @@ const contents = ref([
 .container {
   display: flex;
   flex-direction: column;
-  gap: 10px;
-  padding: 60px;
+  gap: 20px;
+  width: 100%;
+  padding: 40px 60px;
 }
 
 .box {
   display: flex;
-  gap: 10px;
-  border-radius: 20px;
+  gap: 20px;
+  border-radius: 80px;
   background-color: var(--roxo-medio);
-  height: 330px;
+  max-width: 1200px;
+  margin: 0 auto;
+  flex: 1;
+  max-height: 280px;
+}
+
+.bgamarelo {
+  background-color: var(--amarelo);
+}
+
+.tituloroxo {
+  color: var(--roxo-medio);
+}
+
+.botaoroxo {
+  background-color: var(--roxo-medio);
+  color: var(--amarelo);
 }
 
 img {
-  border-radius: 20px;
+  border-radius: 60px;
+  object-fit: cover;
+  object-position: center top;
+  width: 250px;
+}
+
+.imgLogo {
+  padding: 20px 12.5px;
+  width: 320px;
 }
 
 .conteudo {
   display: flex;
   flex-direction: column;
   padding: 10px;
+  justify-content: center;
+  gap: 5px
 }
 
 a {
   width: 250px;
 }
+
 </style>
