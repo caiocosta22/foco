c3ba7786f8a003dce27a3059857777ba9d2bdcea
Caio Costa <caiocosta22@gmail.com>
2024-12-15 19:45:56 -0300

    adicionando novas paginas

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,34 @@ const routes = [
       { path: "", component: () => import("src/pages/Home.vue") }
     ]
   },
+  {
+    path: "/servico",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/Servico.vue") }
+    ]
+  },
+  {
+    path: "/blog",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/Blog.vue") }
+    ]
+  },
+  {
+    path: "/contato",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/Contato.vue") }
+    ]
+  },
+  {
+    path: "/sobre",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("src/pages/Sobre.vue") }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
