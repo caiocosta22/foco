f73b9fad484cb983a2a09e35c2f1f75f87f39a79
Caio Costa <caiocosta22@gmail.com>
2024-12-15 21:19:22 -0300

    adicionando pagina de contato

--- src/pages/Contato.vue
+++ src/pages/Contato.vue
@@ -1,11 +1,110 @@
 <script setup>
+import { ref } from "vue";
+const inputs = ref([
+  {
+    titulo: "Seu nome*",
+    model: "",
+    label: "Digite seu nome aqui"
+  },
+  {
+    titulo: "Seu e-mail*",
+    model: "",
+    label: "Digite seu e-mail aqui"
+  },
+  {
+    titulo: "Seu número de telefone*",
+    model: "",
+    label: "Digite seu número de telefone aqui"
+  },
+  {
+    titulo: "Assunto da mensagem",
+    model: "",
+    label: "Como podemos te ajudar hoje?"
+  },
+  {
+    titulo: "Mensagem",
+    model: "",
+    label: "Escreva sua mensagem aqui",
+    fill: true
+  }
+]);
 </script>
 
 <template>
-<span>
-  a
-</span>
+<section class="form">
+  <div>
+    <h1 class="texto-roxo text-xl text-bold">
+    Entre em contato
+  </h1>
+  <h2 class="texto-roxo text-xlg">
+    Estamos aqui para ajudar e esclarecer <br> qualquer dúvida que possa surgir.
+  </h2>
+  </div>
+  <div class="inputs">
+    <div class="input" v-for="input in inputs" :key="input">
+      <p class="texto-roxo text-md">
+        {{ input.titulo }}
+      </p>
+      <q-input
+      :model="input.model"
+      :label="input.label"
+      color="primary"
+      label-color="primary"
+      :class="{fill: input.fill}"
+      />
+    </div>
+  </div>
+  <div class="flex-row">
+    <span class="texto-roxo text-sm text-left">
+      Estamos comprometidos com a proteção da sua privacidade.<br>Garantimos que nenhuma informação será coletada sobre<br>você sem o seu consentimento explícito.
+    </span>
+    <a href=""
+    class="botao-roxo">
+      <span class="text-md texto-amarelo">
+        Enviar
+      </span>
+    </a>
+  </div>
+</section>
 </template>
 
 <style scoped>
+.form {
+  background-color: #ffffff;
+  border-radius: 0px 0px 100px 100px;
+  display: flex;
+  gap: 40px;
+  flex-direction: column;
+  text-align: center;
+  padding: 60px 60px;
+}
+
+h1, h2 {
+  line-height: 1.2;
+}
+
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.inputs {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 40px;
+  justify-content: center;
+}
+
+.input {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  width: 45%;
+}
+
+.fill{
+  width: 100%
+}
 </style>
