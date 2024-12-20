39b4b46975012c55c05169e48461f6f3e227d3b9
Caio Costa <caiocosta22@gmail.com>
2024-12-20 19:46:24 -0300

    adicionando vue splide

--- src/components/Carousel/Carousel.vue
+++ src/components/Carousel/Carousel.vue
@@ -1,11 +1,37 @@
 <script setup>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/vue-splide/css";
+import { ref } from "vue";
+
+const images = ref([
+  "/images/carousel/editais.png",
+  "/images/carousel/cronograma.png",
+  "/images/carousel/feedback.png",
+  "/images/carousel/questoes.png",
+  "/images/carousel/saude.png",
+  "/images/carousel/mapeamento.png"
+]);
+
+const options = {
+  perPage: 3,
+  padding: "20px",
+  gap: "20px",
+  rewind: true,
+  autoplay: true,
+  pagination: false,
+  arrows: false
+};
 </script>
 
 <template>
-<span>
-  a
-</span>
+  <Splide :options="options" aria-label="Opções Foco"
+  class="q-pt-md">
+    <SplideSlide v-for="image in images" :key="image">
+      <img :src="image" alt="Foto">
+    </SplideSlide>
+  </Splide>
 </template>
 
 <style scoped>
+
 </style>
