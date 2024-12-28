14744b407cd01a69b5bf1bd246d9f511bee332b2
Caio Costa <caiocosta22@gmail.com>
2024-12-27 23:30:06 -0300

    ajuste lista

--- src/components/Menu/Sidebar.vue
+++ src/components/Menu/Sidebar.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { icon } from "@fortawesome/fontawesome-svg-core";
-import { IconSet } from "quasar";
 import { ref } from "vue";
 
 const toggle = ref(false);
-
+const itemativo = ref(false);
 const drawer = ref(false);
 
 const itens = ref([
@@ -60,10 +58,38 @@ class="bg-primary">
       </div>
     </div>
     <div class="lista">
-      <div class="flex align-center">
-        <img src="/icons/sidebar/edital.svg" alt="Icone Edital">
-        <h3 class="text-md text-white">Meus editais</h3>
-        <img src="/icons/sidebar/downarrow.svg" alt="Icone seta para baixo">
+      <div>
+        <div class="flex align-center cursor-pointer"
+        @click="itemativo = !itemativo">
+          <img src="/icons/sidebar/edital.svg" alt="Icone Edital">
+          <h3 class="text-md text-white">Meus editais</h3>
+          <img src="/icons/sidebar/downarrow.svg" alt="Icone seta para baixo" class="q-mt-sm arrow"
+          :class="{ rotated: itemativo }">
+        </div>
+        <transition name="slide">
+          <ul class="itens" v-if="itemativo">
+            <li class="flex align-center">
+              <img src="/icons/sidebar/dot.png" alt=""
+              class="q-mt-sm">
+              <p class="text-sm">
+                TSE Unificado
+              </p>
+            </li>
+            <li class="flex align-center">
+              <img src="/icons/sidebar/dot.png" alt=""
+              class="q-mt-sm">
+              <p class="text-sm">
+                UFC | 2024
+              </p>
+            </li>
+            <li class="flex align-center">
+              <img src="/icons/sidebar/plus.svg" alt="">
+              <p class="text-sm">
+                Novo Edital
+              </p>
+            </li>
+          </ul>
+      </transition>
       </div>
       <div class="flex align-center"
       v-for="item in itens" :key="item"
@@ -130,5 +156,48 @@ h3 {
   gap: 60px;
   display: flex;
   flex-direction: column;
+  transition: 0.5s ease-in-out;
+}
+
+.itens {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding-left: 50px;
+  padding-top: 10px;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.2s ease-in-out;
+}
+
+.slide-enter-from {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.slide-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.slide-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.slide-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+/* Rotação da seta */
+.arrow {
+  transition: transform 0.3s ease-in-out;
+  transform: rotate(-90deg);
+}
+
+.arrow.rotated {
+  transform: rotate(0deg);
 }
 </style>
