cb466213537f11c64931bea82cf965c7ed71f061
Caio Costa <caiocosta22@gmail.com>
2024-12-29 21:23:45 -0300

    ajustes botoes e texto

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -2,56 +2,61 @@
 import { ref } from "vue";
 import { TypingEffect } from "src/router";
 
+const selected = ref(false);
 </script>
 
 <template>
-<div class="flex">
-  <img src="/images/logomin.svg" alt="Logo Foco" class="logo">
-  <div>
-    <h2 class="text-white text-sm q-mb-md">
-    <TypingEffect text="Edital recebido com sucesso!<br>Detectei que este é o edital do TSE Unificado 2024.
-Estou pronta para te ajudar a organizar seus estudos<br><br>O que você gostaria de fazer agora?"
-    :speed="160"/>
-    </h2>
+<section class="container">
+  <div class="texto-inicial">
+    <img src="/images/logomin.svg" alt="Logo Foco" class="logo">
+    <div>
+      <h2 class="text-white text-sm q-mb-md">
+      <TypingEffect text="<b>Edital recebido com sucesso!</b><br>Detectei que este é o edital do <b>TSE Unificado 2024.</b><br>Estou pronta para te ajudar a organizar seus estudos<br><br>O que você gostaria de fazer agora?"
+      :speed="50"/>
+      </h2>
+    </div>
   </div>
-</div>
-<section>
-<div class="flex flex-wrap q-my-md">
-  <a href="" class="botao-roxo text-sm text-white">
-    Criar um cronograma de <br> estudos personalizado
-  </a>
-  <a href="" class="botao-roxo text-sm text-white">
-    Tirar dúvidas<br> sobre o edital
-  </a>
-  <a href="" class="botao-roxo text-sm text-white">
-    Listar os principais <br> conteudos cobrados
-  </a>
-  <a href="" class="botao-roxo text-sm text-white">
-    Mais
-  </a>
-</div>
+  <section>
+    <div class="flex flex-wrap q-my-md">
+      <a class="botao-roxo text-sm text-white">
+        Criar um cronograma de <br> estudos personalizado
+      </a>
+      <a class="botao-roxo text-sm text-white">
+        Tirar dúvidas<br> sobre o edital
+      </a>
+      <a class="botao-roxo text-sm text-white">
+        Listar os principais <br> conteudos cobrados
+      </a>
+      <a class="botao-roxo text-sm text-white justify-center">
+        Mais
+      </a>
+    </div>
+  </section>
+  <section v-if="selected">
+    <div class="q-my-md flex">
+      <a href="" class="botao-amarelo texto-roxo-escuro text-sm">
+        Criar um cronograma de estudos personalizados
+      </a>
+    </div>
+    <div class="flex">
+      <img src="/images/logomin.svg" alt="Logo Foco" class="logo">
+      <div>
+        <p class="text-white text-sm q-mb-md">
+        <TypingEffect text="Certo, vamos criar um cronograma personalizado para o TSE Unificado 2024.<br>Para ajustar o plano às suas necessidades, preciso de algumas informações:<br>1.Qual o cargo que quer estudar?<br>2.Quantas horas por dia você pode dedicar aos estudos?<br>3.Quantos dias na semana você planeja estudar?<br>4.Você prefere alternar disciplinas ou focar em uma por vez?<br><br>Assim que me passar essas informações, montarei seu cronograma sob medida" :speed="160"/>
+        </p>
+      </div>
+    </div>
+  </section>
 </section>
-<div class="q-my-md flex">
-  <a href="" class="botao-amarelo texto-roxo-escuro text-sm">
-    Criar um cronograma de estudos personalizados
-  </a>
-</div>
-<div class="flex">
-  <img src="/images/logomin.svg" alt="Logo Foco" class="logo">
-  <div>
-    <p class="text-white text-sm q-mb-md">
-    <TypingEffect text="Certo, vamos criar um cronograma personalizado para o TSE Unificado 2024.<br>Para ajustar o plano às suas necessidades, preciso de algumas informações:<br>
-1.Qual o cargo que quer estudar?<br>
-2.Quantas horas por dia você pode dedicar aos estudos?<br>
-3.Quantos dias na semana você planeja estudar?<br>
-4.Você prefere alternar disciplinas ou focar em uma por vez?<br><br>Assim que me passar essas informações, montarei seu cronograma sob medida"
-    :speed="160"/>
-    </p>
-  </div>
-</div>
 </template>
 
 <style scoped>
+.container {
+  min-height: 70vh;
+  display: flex;
+  flex-direction: column;
+}
+
 .logo {
   align-self: flex-start;
   padding: 0px 10px;
@@ -68,5 +73,16 @@ Estou pronta para te ajudar a organizar seus estudos<br><br>O que você gostaria
 
 .botao-amarelo {
   align-self: flex-end;
+}
+
+.texto-inicial {
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: 8% 90%;
+}
+
+.justify-center {
+  align-self: center;
+  padding: 20px;
 }
 </style>
