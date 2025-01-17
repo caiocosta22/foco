1c5b8201ea9f7d62269f4263e1e60c00499bcace
Caio Costa <caiocosta22@gmail.com>
2025-01-17 19:53:44 -0300

    comeco de responsividade footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -5,7 +5,7 @@ import { ref } from "vue";
 
 <template>
 <section class="footer">
-  <img src="/images/logofooter.svg" alt="Logo Foco">
+  <img src="/images/logofooter.svg" alt="Logo Foco" class="logo">
   <ul>
     <li>
       <p class="text-md">
@@ -74,5 +74,35 @@ b {
 
 .footer-color {
   color: #E5CAFF;
+}
+
+@media screen and (max-width:1006px) {
+  .text-xl {
+    font-size: 3.5rem;
+  }
+  .text-xlg {
+    font-size: 2rem;
+  }
+  .text-lg {
+    font-size: 1.75rem;
+  }
+  .text-md {
+    font-size: 1.25rem;
+  }
+  .footer {
+    padding: 20px 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .icones {
+    text-align: left;
+  }
+  .icones img {
+    align-self: flex-start;
+  }
+  .logo {
+    align-self: center;
+  }
 }
 </style>
